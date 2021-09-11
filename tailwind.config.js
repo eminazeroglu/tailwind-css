31bc0ac559add40b9d94a580cce2b5e852be4783
eminazeroglu <33575144+eminazeroglu@users.noreply.github.com>
2021-09-11 19:05:06 +0400

    fix: Add Plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+const _ = require('lodash')
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
     //mode: 'jit',
     purge: ["./dist/**/*.html"],
@@ -8,5 +11,22 @@ module.exports = {
     variants: {
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        plugin(function ({ addUtilities, theme, e }) {
+            const calcUtilities = _.map(theme('spacing'), (value, key) => {
+                return {
+                    [`.${e(`calc-h-full-${key}`)}`]: {
+                        height: `calc(100% - ${value})`
+                    },
+                    [`.${e(`calc-w-full-${key}`)}`]: {
+                        width: `calc(100% - ${value})`
+                    }
+                }
+            })
+
+            addUtilities(calcUtilities, {
+                variants: ['responsive', 'hover', 'focus'],
+            })
+        })
+    ],
 }
